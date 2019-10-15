b5e87c1f4eeb0895bc3da56274deb6b1367d324a
jeremydmiller <jeremydmiller@yahoo.com>
2019-10-15 08:03:38 -0500

    imported the topological sort code from Marten. Closes GH-17

--- src/Baseline/EnumerableExtensions.cs
+++ src/Baseline/EnumerableExtensions.cs
@@ -9,6 +9,49 @@ namespace Baseline
     public static class GenericEnumerableExtensions
     {
         /// <summary>
+        /// Performs a topological sort on the enumeration based on dependencies
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="dependencies"></param>
+        /// <param name="throwOnCycle"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> TopologicalSort<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> dependencies, bool throwOnCycle = true)
+        {
+            var sorted = new List<T>();
+            var visited = new HashSet<T>();
+
+            foreach (var item in source)
+            {
+                Visit(item, visited, sorted, dependencies, throwOnCycle);
+            }
+
+            return sorted;
+        }
+
+        private static void Visit<T>(T item, ISet<T> visited, ICollection<T> sorted, Func<T, IEnumerable<T>> dependencies, bool throwOnCycle)
+        {
+            if (visited.Contains(item))
+            {
+                if (throwOnCycle && !sorted.Contains(item))
+                {
+                    throw new Exception("Cyclic dependency found");
+                }
+            }
+            else
+            {
+                visited.Add(item);
+
+                foreach (var dep in dependencies(item))
+                {
+                    Visit(dep, visited, sorted, dependencies, throwOnCycle);
+                }
+
+                sorted.Add(item);
+            }
+        }
+        
+        /// <summary>
         /// Adds the value to the list if it does not already exist
         /// </summary>
         /// <typeparam name="T"></typeparam>
