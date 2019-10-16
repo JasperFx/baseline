af4fabe6bab83d068d4d4340473c4b7a83d6792c
jeremydmiller <jeremydmiller@yahoo.com>
2019-10-16 12:07:36 -0500

    more Xml comments in the type scanning code

--- src/BaselineTypeDiscovery/TypeClassification.cs
+++ src/BaselineTypeDiscovery/TypeClassification.cs
@@ -5,11 +5,34 @@ namespace BaselineTypeDiscovery
     [Flags]
     public enum TypeClassification : short
     {
+        /// <summary>
+        /// All types
+        /// </summary>
         All = 0,
+        
+        /// <summary>
+        /// Open generic types
+        /// </summary>
         Open = 1,
+        
+        /// <summary>
+        /// "Closed" or non-generic types
+        /// </summary>
         Closed = 2,
+        
+        /// <summary>
+        /// Only Interface types
+        /// </summary>
         Interfaces = 4,
+        
+        /// <summary>
+        /// Abstract classes
+        /// </summary>
         Abstracts = 8,
+        
+        /// <summary>
+        /// Concrete classes
+        /// </summary>
         Concretes = 16
     }
 }
--- src/BaselineTypeDiscovery/TypeRepository.cs
+++ src/BaselineTypeDiscovery/TypeRepository.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace BaselineTypeDiscovery
 {
     // Really only tested in integration with other things
+    /// <summary>
+    /// Mechanism to analyze and scan assemblies for exported types
+    /// </summary>
     public static class TypeRepository
     {
         private static ImHashMap<Assembly, Task<AssemblyTypes>> _assemblies = ImHashMap<Assembly, Task<AssemblyTypes>>.Empty;
@@ -32,6 +35,11 @@ namespace BaselineTypeDiscovery
             }
         }
 
+        /// <summary>
+        /// Query for all assemblies that could not be scanned, usually because
+        /// of missing dependencies
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<AssemblyTypes> FailedAssemblies()
         {
             var tasks = _assemblies.Enumerate().Select(x => x.Value).ToArray();
@@ -40,6 +48,11 @@ namespace BaselineTypeDiscovery
             return tasks.Where(x => x.Result.Record.LoadException != null).Select(x => x.Result);
         }
 
+        /// <summary>
+        /// Scan a single assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
         public static Task<AssemblyTypes> ForAssembly(Assembly assembly)
         {
             if (_assemblies.TryFind(assembly, out var types))
@@ -53,6 +66,12 @@ namespace BaselineTypeDiscovery
             return types;
         }
 
+        /// <summary>
+        /// Find types matching a certain criteria from an assembly
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         public static Task<TypeSet> FindTypes(IEnumerable<Assembly> assemblies, Func<Type, bool> filter = null)
         {
             var tasks = assemblies.Select(ForAssembly).ToArray();
@@ -62,7 +81,14 @@ namespace BaselineTypeDiscovery
             });
         }
 
-
+        
+        /// <summary>
+        /// Find types matching a certain criteria and TypeClassification from an Assembly
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="classification"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         public static Task<IEnumerable<Type>> FindTypes(IEnumerable<Assembly> assemblies,
             TypeClassification classification, Func<Type, bool> filter = null)
         {
@@ -72,6 +98,7 @@ namespace BaselineTypeDiscovery
             return Task.Factory.ContinueWhenAll(tasks, results => results.SelectMany(x => x.Result));
         }
 
+        
         public static Task<IEnumerable<Type>> FindTypes(Assembly assembly, TypeClassification classification,
             Func<Type, bool> filter = null)
         {
