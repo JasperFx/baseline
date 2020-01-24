048f5008674670b9cb65789ee3a3bd1555492d37
Othman DAID <othman.daid.whyse@outlook.fr>
2020-01-24 19:44:12 +0100

    Add Widget5Caller and perform a call from Widget3

---
+++ src/Widgets5/Widget5Caller.cs
@@ -0,0 +1,18 @@
+﻿using BaselineTypeDiscovery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Widgets5
+{
+    public class Widget5Caller
+    {
+        public static Assembly Calling()
+        {
+            return CallingAssembly.Find();
+        }
+    }
+}
