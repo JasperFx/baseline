8aa67ebb495b7c653781e2792ab70afb79585652
Jeremy D. Miller <jeremydmiller@yahoo.com>
2021-06-25 10:27:06 -0500

    adding FindMembers back to Baseline. Baseline 3.2.0

---
+++ src/Baseline.Testing/Expressions/FindMembersTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Baseline.Expressions;
+using Shouldly;
+using Xunit;
+
+namespace Baseline.Testing.Expressions
+{
+    public class FindMembersTester
+    {
+        [Fact]
+        public void find_a_single_property()
+        {
+            Expression<Func<Target, object>> expression = x => x.Color;
+            FindMembers.Determine(expression).Single().Name.ShouldBe(nameof(Target.Color));
+        }
+        
+        [Fact]
+        public void find_a_single_field()
+        {
+            Expression<Func<Target, object>> expression = x => x.StringField;
+            FindMembers.Determine(expression).Single().Name.ShouldBe(nameof(Target.StringField));
+        }
+
+        [Fact]
+        public void find_multiple_properties()
+        {
+            Expression<Func<Target, object>> expression = x => x.Inner.Color;
+            var members = FindMembers.Determine(expression);
+            members.Select(x => x.Name).ShouldHaveTheSameElementsAs("Inner", "Color");
+        }
+    }
+}
---
+++ src/Baseline/Expressions/FindMembers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Baseline.Expressions
+{
+    /// <summary>
+    /// Use to find the MemberInfo called within an expression
+    /// </summary>
+    public class FindMembers: ExpressionVisitor
+    {
+        internal static readonly PropertyInfo ArrayLength = typeof(Array).GetProperty(nameof(Array.Length));
+        
+        public static MemberInfo Member<T>(Expression<Func<T, object>> expression)
+        {
+            var finder = new FindMembers();
+            finder.Visit(expression);
+
+            return finder.Members.LastOrDefault();
+        }
+
+        public readonly IList<MemberInfo> Members = new List<MemberInfo>();
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            Members.Insert(0, node.Member);
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.Name == "Count" && node.Method.ReturnType == typeof(int))
+            {
+                Members.Insert(0, ArrayLength);
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        protected sealed override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.ArrayLength)
+            {
+                Members.Insert(0, ArrayLength);
+            }
+
+            return base.VisitUnary(node);
+        }
+
+        /// <summary>
+        /// Find any MemberInfo within an expression
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static MemberInfo[] Determine(Expression expression)
+        {
+            var visitor = new FindMembers();
+
+            visitor.Visit(expression);
+
+
+
+
+            return visitor.Members.ToArray();
+        }
+    }
+}
